7699a2f64489ae46bc5348ddf50e2eb75d1e1462
jamesattwood <jamesattwood@zap-map.com>
2022-11-08 23:03:10 +0000

    thanks for registering page

---
+++ thanks_for_registering.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Thanks for registering</title>
+
+	<style type="text/css">
+		
+		body {
+
+		background-color: #1A4D2E;
+        font-family: Lucida Console;
+        color: #FAF3E3;
+		}
+
+		h1 {
+        background-color: #FAF3E3;
+        padding: 5px 10px;
+        position: center;
+        margin-top: 50px;
+        color: #000000;
+        max-width: min(400px, 50%);
+
+    	}
+
+    	p {
+    		border-style: outset;
+      		border-color: #FAF3E3;
+      		max-width: min(90%,600px);
+      		padding: 10px;
+    	}
+
+    	a {
+    		text-decoration:none;
+        	color: #000000;
+        	background-color:#FF9F29;
+        	height: 30px;
+        	width: 50px;
+        	padding: 5px;
+        	border-style: outset;
+        	border-color: #FAF3E3;
+    	}
+
+    	a:hover {
+    		background-color: #FAF3E3;
+        	color: #000000;
+
+    	}
+
+
+	</style>
+
+
+
+</head>
+<body>
+
+	<h1>Welcome to Avoid the Algorithm</h1>
+
+	<p>Thank you for registering. Your email and password have been emailed to you. These will be your login details enabling you to react to each article you read.</p>
+	<br>
+	<a href="login_page.php">Login in to your account here.</a>
+
+</body>
+</html>
