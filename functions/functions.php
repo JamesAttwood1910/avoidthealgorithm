7cae7377f9169456425b696bf2556e5d3197e1ed
jamesattwood <jamesattwood@zap-map.com>
2022-11-27 17:04:30 +0000

    function update

--- functions/functions.php
+++ functions/functions.php
@@ -9,14 +9,19 @@ Data base connection
 
 #mongodb://localhost:27017/
 
-function connect_mongodb($host = "mongodb+srv://james_attwood:7tadZOaBzo94zUeZ@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority") {
+// function connect_mongodb($host = "mongodb+srv://james_attwood:7tadZOaBzo94zUeZ@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority") {
 
-		$connection = new MongoDB\Client($host);
+// 		$connection = new MongoDB\Client($host);
 
-		return $connection;
-	}
+// 		return $connection;
+// 	}
 
-
+function connect_mongodb($host = 'mongodb+srv://james_attwood:7tadZOaBzo94zUeZ@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority') {
+	$serverApi = new ServerApi(ServerApi::V1);
+	$connection = new MongoDB\Client(
+    $host, [], ['serverApi' => $serverApi]);
+    return $connection
+}
 ####################################################
 
 
