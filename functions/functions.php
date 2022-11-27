82890b09f46387dbe2d8fc469fdba9233c64c6aa
jamesattwood <jamesattwood@zap-map.com>
2022-11-27 16:22:44 +0000

    commit passowrd change for mongo connect

--- functions/functions.php
+++ functions/functions.php
@@ -9,7 +9,7 @@ Data base connection
 
 #mongodb://localhost:27017/
 
-function connect_mongodb($host = "mongodb+srv://james_attwood:1910Sta191018@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority") {
+function connect_mongodb($host = "mongodb+srv://james_attwood:<password>@avoidthealgorithm1.jsu1na6.mongodb.net/test") {
 
 		$connection = new MongoDB\Client($host);
 
