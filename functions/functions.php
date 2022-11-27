bb1761478d85e74e1d69989d813864a44bfc88f1
jamesattwood <jamesattwood@zap-map.com>
2022-11-27 16:59:49 +0000

    test

--- functions/functions.php
+++ functions/functions.php
@@ -9,7 +9,7 @@ Data base connection
 
 #mongodb://localhost:27017/
 
-function connect_mongodb($host = "mongodb://localhost:27017/") {
+function connect_mongodb($host = "mongodb+srv://james_attwood:7tadZOaBzo94zUeZ@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority") {
 
 		$connection = new MongoDB\Client($host);
 
