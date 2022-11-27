03c4bdb5547866322de148a1a582db5aee158ce5
jamesattwood <jamesattwood@zap-map.com>
2022-11-27 19:50:35 +0000

    local host

--- functions/functions.php
+++ functions/functions.php
@@ -9,19 +9,19 @@ Data base connection
 
 #mongodb://localhost:27017/
 
-// function connect_mongodb($host = "mongodb+srv://james_attwood:7tadZOaBzo94zUeZ@avoidthealgorithm1.jsu1na6.mongodb.net/test") {
+function connect_mongodb($host = "mongodb://localhost:27017/") {
 
-// 		$connection = new MongoDB\Client($host);
+		$connection = new MongoDB\Client($host);
 
-// 		return $connection;
-// 	}
-
-	function connect_mongodb($host = 'mongodb+srv://admin:TM4S3PwGsROwUJbT@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority') {
-		$serverApi = new ServerApi(ServerApi::V1);
-		$connection = new MongoDB\Client(
-	    $host, [], ['serverApi' => $serverApi]);
-	    return $connection;
+		return $connection;
 	}
+
+	// function connect_mongodb($host = 'mongodb+srv://admin:TM4S3PwGsROwUJbT@avoidthealgorithm1.jsu1na6.mongodb.net/?retryWrites=true&w=majority') {
+	// 	$serverApi = new ServerApi(ServerApi::V1);
+	// 	$connection = new MongoDB\Client(
+	//     $host, [], ['serverApi' => $serverApi]);
+	//     return $connection;
+	// }
 ####################################################
 
 
